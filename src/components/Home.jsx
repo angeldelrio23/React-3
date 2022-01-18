f92dc471671d8efe0bdd285c338aaf47480eb79a
angeldelrio23 <angeldelrio23@yahoo.es>
2022-01-18 12:17:57 +0100

    Clase 18/01/2022

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
 
 export function Home() {
   return (
     <div className="main-site">
       <h1>Bienvenido!</h1>
+
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Nombre</th>
+            <th>Primer apellido</th>
+            <th>Nombre usuario</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Angel</td>
+            <td>del Rio</td>
+            <td>@angeldelrio23</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Manuel</td>
+            <td>David</td>
+            <td>@Supermanu545</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Alejandro Fernández</td>
+            <td>@shurklex</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
---
+++ src/data/DatosCoches.js
@@ -0,0 +1,33 @@
+export const TitulosTablaCoches = {
+  id: 'Matricula',
+  field1: 'Marca',
+  field2: 'Modelo',
+  field3: 'Color',
+};
+
+export const DatosTablaCoches = [
+  {
+    matricula: '0000LOL',
+    marca: 'Peugeot',
+    modelo: 'Partner',
+    color: 'Rojo',
+  },
+  {
+    matricula: '7204FGA',
+    marca: 'Hyundai',
+    modelo: 'IX35',
+    color: 'Verde',
+  },
+  {
+    matricula: '5050SOD',
+    marca: 'Mitshubishi',
+    modelo: 'Evo 7',
+    color: 'Blanco',
+  },
+  {
+    matricula: '0450LDL',
+    marca: 'Subaru',
+    modelo: 'Impreza',
+    color: 'Rojo',
+  },
+];
