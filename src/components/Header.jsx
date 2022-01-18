d43431d0a00b1e0c06ca114fec3476ceb4dd1dc8
angeldelrio23 <angeldelrio23@yahoo.es>
2022-01-18 10:10:04 +0100

    10 files modified, 1 added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,13 @@ import './header.css';
 class Header extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      clicked: false,
+    };
+  }
+
+  changeClicked() {
+    this.setState({ clicked: !this.state.clicked });
   }
 
   render() {
@@ -17,34 +24,30 @@ class Header extends React.Component {
             className="logo-image"
             src="https://eu01.edcwb.com/buscador/img/centros/logogrande/7348-a9c730d6b2b644f5b9910364ba6af277.jpg"
           />
+          <i className="fas fa-user-graduate" />
         </div>
-        <div className="fa.futbol" />
-        <ul>
+        <div className="menu-icon" onClick={this.changeClicked.bind(this)}>
+          <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul
+          className={this.state.clicked ? 'navbar-menu active' : 'navbar-menu'}
+        >
           {MenuItems.map((item) => {
             return (
               <li key={item.id}>
-                <Link to={item.path}>{item.title}</Link>
+                <Link
+                  to={item.path}
+                  className="navbar-link"
+                  onClick={this.changeClicked.bind(this)}
+                >
+                  {item.title}
+                </Link>
               </li>
             );
           })}
         </ul>
       </nav>
     );
-  }
-
-  /**
-   * Forma menú nº 1: bucle for
-   */
-  createMenuLinks() {
-    const listComponents = [];
-    for (let i = 0; i < MenuItems.length; i++) {
-      listComponents.push(
-        <li>
-          <Link to={MenuItems[i].path}>{MenuItems[i].title}</Link>
-        </li>
-      );
-    }
-    return listComponents;
   }
 }
 
